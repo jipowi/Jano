ebc91e96ad9642089a0e260e66b0257155941865
paulJimenez666 <paul_jimenez777@hotmail.com>
2016-06-09 22:00:24 -0500

    Pantalla recaudaciones

---
+++ jano_web/src/main/java/ec/com/uce/jano/web/beans/ReporteRecaudacionBean.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2016 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.uce.jano.web.beans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ * <b> Incluir aqui la descripcion de la clase. </b>
+ * 
+ * @author HIPERION
+ * @version 1.0,23/02/2016
+ * @since JDK1.6
+ */
+@ManagedBean
+@ViewScoped
+public class ReporteRecaudacionBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String comprobante;
+	private Date fecha;
+	private String periodo;
+	private String observacion;
+	private String beneficiario;
+	private double valor;
+	private Integer facultad;
+	private Integer dependencia;
+	private Long idAfectacion;
+
+	/**
+	 * @return the periodo
+	 */
+	public String getPeriodo() {
+		return periodo;
+	}
+
+	/**
+	 * @param periodo
+	 *            the periodo to set
+	 */
+	public void setPeriodo(String periodo) {
+		this.periodo = periodo;
+	}
+
+	/**
+	 * @return the comprobante
+	 */
+	public String getComprobante() {
+		return comprobante;
+	}
+
+	/**
+	 * @param comprobante
+	 *            the comprobante to set
+	 */
+	public void setComprobante(String comprobante) {
+		this.comprobante = comprobante;
+	}
+
+	/**
+	 * @return the fecha
+	 */
+	public Date getFecha() {
+		return fecha;
+	}
+
+	/**
+	 * @param fecha
+	 *            the fecha to set
+	 */
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	/**
+	 * @return the observacion
+	 */
+	public String getObservacion() {
+		return observacion;
+	}
+
+	/**
+	 * @param observacion
+	 *            the observacion to set
+	 */
+	public void setObservacion(String observacion) {
+		this.observacion = observacion;
+	}
+
+	/**
+	 * @return the beneficiario
+	 */
+	public String getBeneficiario() {
+		return beneficiario;
+	}
+
+	/**
+	 * @param beneficiario
+	 *            the beneficiario to set
+	 */
+	public void setBeneficiario(String beneficiario) {
+		this.beneficiario = beneficiario;
+	}
+
+	/**
+	 * @return the valor
+	 */
+	public double getValor() {
+		return valor;
+	}
+
+	/**
+	 * @param valor
+	 *            the valor to set
+	 */
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	/**
+	 * @return the facultad
+	 */
+	public Integer getFacultad() {
+		return facultad;
+	}
+
+	/**
+	 * @param facultad
+	 *            the facultad to set
+	 */
+	public void setFacultad(Integer facultad) {
+		this.facultad = facultad;
+	}
+
+	/**
+	 * @return the dependencia
+	 */
+	public Integer getDependencia() {
+		return dependencia;
+	}
+
+	/**
+	 * @param dependencia
+	 *            the dependencia to set
+	 */
+	public void setDependencia(Integer dependencia) {
+		this.dependencia = dependencia;
+	}
+
+	/**
+	 * @return the idAfectacion
+	 */
+	public Long getIdAfectacion() {
+		return idAfectacion;
+	}
+
+	/**
+	 * @param idAfectacion the idAfectacion to set
+	 */
+	public void setIdAfectacion(Long idAfectacion) {
+		this.idAfectacion = idAfectacion;
+	}
+
+	
+
+}
